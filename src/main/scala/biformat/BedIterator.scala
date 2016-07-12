c24986c398c77c266ca141ae8c7738cfa69a35ea
yuto <ahne.ahne.ahne@gmail.com>
2016-07-12 22:30:52 +0900

    fix BedLine.toString

--- src/main/scala/biformat/BedIterator.scala
+++ src/main/scala/biformat/BedIterator.scala
@@ -47,16 +47,21 @@ object BedIterator {
     *
     */
 
-  case class BedLine(chr: String, start: Int, end: Int, name: String = "", score: Int = 0, strand: Char = '+',
-                     thickStart: Int = 0, thickEnd: Int = 0, itemRgb: (Char, Char, Char) =
-                     (Char.MinValue,Char.MaxValue,Char.MinValue) , blockCount: Int = 0,
+  case class BedLine(chr: String, start: Int, end: Int, name: String = "", score: Int = 0, strand: Char = '.',
+                     thickStart: Int = 0, thickEnd: Int = 0, itemRgb: (Int, Int, Int) =
+                     (0,0,0) , blockCount: Int = 0,
                      blockSize: Array[Int] = Array.empty, blockStarts: Array[Int] = Array.empty) extends Block {
 
     def length = end - start
     def appendableWith(that: Block): Boolean = false
-    override def toString = chr + '\t' + start + '\t' + end + '\t' + name + '\t' + score + '\t' + strand + '\t' +
-      thickStart + '\t' + thickEnd + '\t' + itemRgb._1 + ',' + itemRgb._2 + ',' + itemRgb._3 + ',' + '\t' +
-      blockCount + '\t' + blockSize.mkString(",") + '\t' + blockStarts.mkString(",")
+    override def toString = {
+      val tmp = chr + '\t' + start + '\t' + end + '\t' + name + '\t' + score + '\t' + strand + '\t' +
+        thickStart + '\t' + thickEnd + '\t' + itemRgb._1 + ',' + itemRgb._2 + ',' + itemRgb._3 + ',' + '\t' +
+        blockCount
+      if(blockSize.nonEmpty) tmp + '\t' + blockSize.mkString(",") + '\t' + blockStarts.mkString(",")
+      else tmp
+    }
+
     def hasOverlap(that:BedLine): Boolean = this.start < that.end && this.end > that.start
   }
 
@@ -66,9 +71,9 @@ object BedIterator {
     def apply(line: String, sep: String): BedLine = {
       val ps = line.split(sep)
       if(ps.length == 12){
-        val rgbbase = ps(8).split(',')
+        val rgbbase = ps(8).split(',').map(_.toInt)
         BedLine(ps(0), ps(1).toInt, ps(2).toInt, ps(3), ps(4).toInt, ps(5).head, ps(6).toInt, ps(7).toInt,
-          (rgbbase(0).head, rgbbase(1).head,rgbbase(2).head), ps(9).toInt, ps(10).split(',').map(_.toInt),
+          (rgbbase(0), rgbbase(1),rgbbase(2)), ps(9).toInt, ps(10).split(',').map(_.toInt),
           ps(11).split(',').map(_.toInt))
       }
       else
